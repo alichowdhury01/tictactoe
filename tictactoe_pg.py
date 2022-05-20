b940ffc6f024f6ad61f5137f80e7a1c4b82d95d4
alichowdhury01 <alichowdhury01@gmail.com>
2022-05-20 10:22:06 -0400

    Update tictactoe_pg.py
    Player turn and symbol created

--- tictactoe_pg.py
+++ tictactoe_pg.py
@@ -1,20 +1,35 @@
-import pygame
-import sys
+import pygame, sys
+import numpy as np
+
 
 pygame.init()
 
 WIDTH = 600
 HEIGHT = 600
 LINE_WIDTH = 15
+BOARD_ROW = 3
+BOARD_COL = 3
+CIRCLE_RADIUS = 60
+CIRCLE_WIDTH = 15
+CROSS_WIDTH = 25
+SPACE = 55
 
 
 # Color
 BG_COLOR = (28, 170, 156)
+RED = (255, 0, 0)
 LINE_COLOR = (23, 145, 135)
+CIRCLE_COLOR = (239, 231, 200)
+CROSS_COLOR = (66, 66, 66)
+
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Tic Tac Toe")
 screen.fill(BG_COLOR)
+
+# Board
+board = np.zeros((BOARD_ROW, BOARD_COL))
+
 
 def draw_lines():
 
@@ -30,13 +45,56 @@ def draw_lines():
     # Vertical 2
     pygame.draw.line(screen, LINE_COLOR, (400, 0), (400, 600), LINE_WIDTH)
 
+def draw_symbol():
+    for row in range(BOARD_ROW):
+        for col in range(BOARD_COL):
+            if board[row][col] == 1:
+                pygame.draw.circle(screen, CIRCLE_COLOR, (int(col * 200 + 100), int(row * 200 + 100)), CIRCLE_RADIUS, CIRCLE_WIDTH)
+            elif board[row][col] == 2:
+                pygame.draw.line(screen, CROSS_COLOR, (col * 200 + SPACE, row * 200 + 200 - SPACE), (col * 200 + 200 - SPACE, row * 200 + SPACE), CROSS_WIDTH)
+                pygame.draw.line(screen, CROSS_COLOR, (col * 200 + SPACE, row * 200 + SPACE), (col * 200 + 200 - SPACE, row * 200 + 200 - SPACE), CROSS_WIDTH)
+
+
+def player_moves(row, col, player):
+    board[row][col] = player
+
+def empty_cell(row, col):
+    return board[row][col] == 0
+
+def is_board_full():
+    for row in range(BOARD_ROW):
+        for col in range(BOARD_COL):
+            if board[row][col] == 0:
+                return False
+    return True
 
 draw_lines()
+
+player = 1
 
 # mainloop
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+
+            mouseX = event.pos[0] # x
+            mouseY = event.pos[1] # y
+
+            clicked_row = int(mouseY // 200)
+            clicked_col = int(mouseX // 200)
+
+            if empty_cell(clicked_row, clicked_col):
+                if player == 1:
+                    player_moves(clicked_row, clicked_col, 1)
+                    player = 2
+
+                elif player == 2:
+                    player_moves(clicked_row, clicked_col, 2)
+                    player = 1
+
+                draw_symbol()
 
     pygame.display.update()
